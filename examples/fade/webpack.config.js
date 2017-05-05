ac46bcd73d15aefe8ed4eb7b8b9827924ebb8170
Anthony Du Pont <anthony@epic.net>
2017-05-05 16:03:25 +0200

    :hammer: Reorder Folders

--- examples/fade/webpack.config.js
+++ examples/fade/webpack.config.js
@@ -3,16 +3,16 @@ const path = require('path');
 const del  = require('del');
 
 // Clean Up Build
-del(path.resolve(__dirname, 'build'), { force: true });
+del(path.resolve(__dirname, 'build/js'), { force: true });
 
 // Configuration
 module.exports = {
   entry: {
-    main: path.resolve(__dirname, 'src/main.js'),
+    main: path.resolve(__dirname, 'src/js/main.js'),
   },
   output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: '[name].js',
+    path: path.resolve(__dirname, 'build/js'),
+    filename: '[name].min.js',
   },
   module: {
     rules: [
@@ -34,4 +34,5 @@ module.exports = {
     ],
     extensions: ['.js'],
   },
+  devtool: 'source-map',
 };
